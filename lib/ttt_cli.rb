ff40614dc0d91869f44d225b826a5dbb1a9de72c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-29 06:13:03 +0000

    automatically backed up by learn

--- lib/ttt_cli.rb
+++ lib/ttt_cli.rb
@@ -3,28 +3,21 @@ class TTT_CLI
   end
 
   def start
-    input = ' '
+    input = ""
      puts "Welcome to Tic-Tac-Toe!"
      puts "How many players?(0,1,2)"
-      #  Please make a selection from bellow using your keybord:\n
-      #    Changes your mind? Please type 'exit'\n
-      #    Enter 1 to Play vs. the Unbeatable Machine!!!!\n
-      #    Enter 2 to Play with a Friend\n
-      #    Enter 3 for AI vs. AI\n"
-
      input = gets.strip
 
      case input
-     when '1'
+     when "1"
        single_player
-     when '2'
-       Game.new(Players::Human.new('X'), Players::Human.new('O'), Board.new).play
+     when "2"
+       Game.new(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
        play_again?
-     when '0'
-       Game.new(Players::Computer.new('X'), Players::Computer.new('O'), Board.new).play
+     when "0"
+       Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new).play
        play_again?
       end
-      #play_again?
    end
 
  def single_player
